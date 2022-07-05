2d0f80289373b442705d3215144e9fb2de3eee8d
romofel <81869916+romofel@users.noreply.github.com>
2022-07-05 13:35:33 +0000

    Use default import rather than named import for context

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { ThemeProvider } from './contexts/theme'
+import ThemeContext from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <Router>
-      <ThemeProvider value={theme}>
+      <ThemeContext.Provider value={theme}>
         <div className={theme}>
           <div className='container'>
             <Nav toggleTheme={toggleTheme} />
@@ -31,7 +31,7 @@ function App() {
             </React.Suspense>
           </div>
         </div>
-      </ThemeProvider>
+      </ThemeContext.Provider>
     </Router>
   );
 }
