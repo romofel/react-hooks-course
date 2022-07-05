e1ea1f6c5ceee639fc1c390c6fb0ddaff414cb10
romofel <81869916+romofel@users.noreply.github.com>
2022-07-05 15:34:05 +0000

    Refactor PlayerPreview to use context hook

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -79,32 +79,30 @@ PlayerInput.propTypes = {
 }
 
 function PlayerPreview({ username, onReset, label }) {
+  const theme = useContext(ThemeContext);
+  
   return (
-    <ThemeConsumer>
-      {({ theme }) => (
-        <div className='column player'>
-          <h3 className='player-label'>{label}</h3>
-          <div className={`row bg-${theme}`}>
-            <div className='player-info'>
-              <img
-                className='avatar-small'
-                src={`https://github.com/${username}.png?size=200`}
-                alt={`Avatar for ${username}`}
-              />
-              <a
-                href={`https://github.com/${username}`}
-                className='link'>
-                {username}
-              </a>
-            </div>
-            <button className='btn-clear flex-center' onClick={onReset}>
-              <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
-            </button>
-          </div>
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className={`row bg-${theme}`}>
+        <div className='player-info'>
+          <img
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a
+            href={`https://github.com/${username}`}
+            className='link'>
+            {username}
+          </a>
         </div>
-      )}
-    </ThemeConsumer>
-  )
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 PlayerPreview.propTypes = {
