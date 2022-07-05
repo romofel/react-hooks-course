ad8d7019317c430495b76d2f48fff5880cf35c3b
romofel <81869916+romofel@users.noreply.github.com>
2022-07-05 13:07:02 +0000

    Refactor theme

--- app/index.js
+++ app/index.js
@@ -10,7 +10,33 @@ const Popular = React.lazy(() => import('./components/Popular'))
 const Battle = React.lazy(() => import('./components/Battle'))
 const Results = React.lazy(() => import('./components/Results'))
 
-class App extends React.Component {
+function App() {
+  const [theme, setTheme] = React.useState('light');
+  const toggleTheme = () => setTheme((theme) => theme === 'light' ? 'dark' : 'light');
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className='container'>
+            <Nav toggleTheme={toggleTheme} />
+
+            <React.Suspense fallback={<Loading />} >
+              <Switch>
+                <Route exact path='/' component={Popular} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+class OldApp extends React.Component {
   state = {
     theme: 'light',
     toggleTheme: () => {
