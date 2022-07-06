8aba1d7766f3acad8af7cceaa787ed63b8782f11
romofel <81869916+romofel@users.noreply.github.com>
2022-07-06 14:47:01 +0000

    Refactor Hover component into a custom hook

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,14 +1,10 @@
 import React from 'react'
 
-export default function Hover ({ children }) {
+export function useHover () {
   const [hovering, setHovering] = React.useState(false);
 
   const mouseOver = () => setHovering(true);
   const mouseOut = () => setHovering(false);
 
-  return (
-    <div onMouseOver={mouseOver} onMouseOut={mouseOut}>
-    {children(hovering)}
-  </div>
-  );
+  return { hovering, mouseOver,  mouseOut };
 }
