705b87b9c4f5d7f54e27851fbd4be7cb4c679db5
romofel <81869916+romofel@users.noreply.github.com>
2022-07-06 12:24:17 +0000

    Refactor Results component to use hooks

--- app/components/Results.js
+++ app/components/Results.js
@@ -47,7 +47,72 @@ ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
 }
 
-export default class Results extends React.Component {
+export default function Results ({ location }) {
+  const [winner, setWinner] = React.useState(null);
+  const [loser, setLoser] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const { playerOne, playerTwo } = queryString.parse(location.search);
+
+    battle([playerOne, playerTwo])
+    .then((players) => {
+      setWinner(players[0]);
+      setLoser(players[1]);
+      setError(null);
+      setLoading(false);
+    })
+    .catch(({ message }) => {
+      setError(message);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <Loading text='Battling' />
+  }
+
+  if (error) {
+    return (
+      <p className='center-text error'>{error}</p>
+    );
+  }
+  
+  return (
+ 
+
+    <React.Fragment>
+      <div className='grid space-around container-sm'>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile}/>
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ProfileList profile={loser.profile}/>
+        </Card>
+      </div>
+      <Link
+        to='/battle'
+        className='btn dark-btn btn-space'>
+          Reset
+      </Link>
+    </React.Fragment>
+  );
+}
+
+class OldResults extends React.Component {
   state = {
     winner: null,
     loser: null,
